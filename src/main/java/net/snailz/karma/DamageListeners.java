b1721ff74e876aa74761a8822fe7dd0e33a92b75
Snailz <snail@snailz.net>
2017-12-05 11:19:53 -0800

    Added Events and KarmaUserManager

---
+++ src/main/java/net/snailz/karma/DamageListeners.java
@@ -0,0 +1,13 @@
+package net.snailz.karma;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class KarmaListeners implements Listener{
+
+    public void onPlayerKill(PlayerDeathEvent e){
+        Player killed = e.getEntity().getPlayer();
+        Player killer = e.
+    }
+}
---
+++ src/main/java/net/snailz/karma/JoinLeaveListeners.java
@@ -0,0 +1,4 @@
+package net.snailz.karma;
+
+public class JoinLeaveListeners {
+}
